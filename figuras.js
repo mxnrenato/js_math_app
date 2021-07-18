de931caae18fdb11bdedda71451c2b488b4714a4
Renato Rivera <maximilianoriveraabad@gmail.com>
2021-07-18 00:24:40 -0500

    Añadiendo figuras.js

--- figuras.js
+++ figuras.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+//Código del cuadrado
+console.group("Cuadrados")
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + " cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + " cm^2");
+
+const areaCuadrado = ladoCuadrado*ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + " cm^3")
+console.groupEnd();
+
+//Codigo del triangulo
+console.group("Cuadrados")
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 6;
+
+console.log("El triangulo posee las siguiente medidas: \n Lado 1: " + ladoTriangulo1 + " \n Lado 2: " + ladoTriangulo2 + " \n Base: " + baseTriangulo + "\n medidas en cm.")
+
+
